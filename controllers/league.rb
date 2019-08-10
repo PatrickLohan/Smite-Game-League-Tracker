23d5756bbf32d619e5c23a269fbce931a03aac47
DatNobleSavage <patrick.lohan@zoho.com>
2019-08-10 22:39:30 +0100

    Adds add team/league

--- controllers/league.rb
+++ controllers/league.rb
@@ -5,3 +5,15 @@ get '/leagues' do
   @leagues = League.all()
   erb(:"leagues/index")
 end
+
+# NEW
+get '/leagues/new' do
+  erb(:"leagues/new")
+end
+
+# CREATE
+post '/leagues' do
+  league = League.new(params)
+  league.save()
+  redirect "/leagues"
+end
--- controllers/team.rb
+++ controllers/team.rb
@@ -5,3 +5,15 @@ get '/teams' do
   @teams = Team.all()
   erb(:"teams/index")
 end
+
+# NEW
+get '/teams/new' do
+  erb(:"teams/new")
+end
+
+# CREATE
+post '/teams' do
+  team = Team.new(params)
+  team.save()
+  redirect "/teams"
+end
