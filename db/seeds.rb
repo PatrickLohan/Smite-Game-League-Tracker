c8eb6455a507ef82f3761f2faec1122c23c83ee9
DatNobleSavage <patrick.lohan@zoho.com>
2019-08-10 16:03:01 +0100

    Adds one league instance and two team instances

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+require_relative("../models/team.rb")
+require_relative("../models/league.rb")
+
+# League.delete_all()
+# Team.delete_all()
+
+league1 = League.new(
+  {
+    "league_name" => "Smite Pro League"
+  }
+)
+
+league1.save()
+
+team1 = Team.new{
+  {
+    "team_name" => "Rival",
+    "league_id" => league1.id
+  }
+}
+
+team1.save()
+
+team2 = Team.new{
+  {
+    "team_name" => "Pittsburgh Knights",
+    "league_id" => league1.id
+  }
+}
+
+team2.save()
