c6eff453c1a8dd9be8d4b528eaa0e0edc1d53004
DatNobleSavage <patrick.lohan@zoho.com>
2019-08-10 17:20:07 +0100

    Fixed { to ( typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require_relative("../models/team.rb")
 require_relative("../models/league.rb")
 
-# League.delete_all()
-# Team.delete_all()
+League.delete_all()
+Team.delete_all()
 
 league1 = League.new(
   {
@@ -12,20 +12,20 @@ league1 = League.new(
 
 league1.save()
 
-team1 = Team.new{
+team1 = Team.new(
   {
     "team_name" => "Rival",
     "league_id" => league1.id
   }
-}
+)
 
 team1.save()
 
-team2 = Team.new{
+team2 = Team.new(
   {
     "team_name" => "Pittsburgh Knights",
     "league_id" => league1.id
   }
-}
+)
 
 team2.save()
